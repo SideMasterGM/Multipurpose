fa1a3721abaf3b9e748e40d676ad5709ba815841
Jerson Martínez <jersonmartinezsm@gmail.com>
2019-05-01 23:02:59 -0600

    Adding new vars about social network and creating several importants methods about services, home, about, etc...

--- app/model/model.php
+++ app/model/model.php
@@ -1,39 +1,32 @@
 <?php
 	class Company {
-		public $basename;
-		public $pagename;
-		public $company;
-		public $address;
+		public $basename, 	$pagename;
+		public $company, 	$address;
 
-		public $phones = array();
-		public $emails = array();
+		public $phones 	= array(),	$emails = array(), 
+			   $phrases = array(),	$home 	= array(), 
+			   $about 	= array(), 	$service= array(), 
+			   $review 	= array();
 
-		public $services;
-		public $payment;
-		public $experience;
+		public $services, 		$payment,
+			   $experience,		$schedule, 
+			   $schedule_short,	$estimates,
+			   $cover,			$license,
+ 			   $lic;
 
-		public $schedule;
-		public $schedule_short;
-
-		public $estimates;
-		public $cover;
-		public $license;
-		public $lic;
-
-		public $googlemap;
-
-		public $facebook;
-		public $twitter;
-		public $linkedin;
+ 		public $googlemap,			$facebook,
+ 			   $twitter,			$linkedin,
+ 			   $googlemybusiness,	$yelp, 
+ 			   $homeadvisor,		$buzzfile,
+			   $businessfinder,		$manta,
+			   $yellowbot,			$houzz,	
+			   $dexknow;
 
 		#SEO
-		public $domain;
-		public $satisfaction;
-		public $locality;
-		public $region;
-		public $street_address;
-		public $bilingual;
-		public $seo_convert;
+		public $domain, 		$satisfaction, 
+			   $locality, 		$region, 
+			   $street_address, $bilingual, 
+			   $seo_convert;
 
 		function __construct(){
 			$this->domain 	= $_SERVER['HTTP_HOST'];
@@ -98,9 +91,8 @@
 
 		public function getEmail($number){
 			if (is_array($this->emails)){
-				if (isset($this->emails[$number - 1])){
+				if (isset($this->emails[$number - 1]))
 					return ['Email' => $this->emails[$number - 1], 'MailRef' => 'mailto:'.$this->emails[$number - 1]];
-				}
 
 				$msg = 'This field does not exist';
 				return ['Email' => $msg, 'MailRef' => $msg];
@@ -125,4 +117,119 @@
 			return ['SEOConvert' => $convert, 'SEOPhone' => '+1'.$convert];
 		}
 
+		public function addPhrase($phrase){
+			array_push($this->phrases, $phrase);
+		}
+
+		public function getPhrase($number){
+			$phrases = $this->phrases;
+
+			if (is_array($phrases)){
+				if (isset($phrases[$number - 1]))
+					return $phrases[$number - 1];
+
+				return 'This field does not exist';
+			}
+
+			return 'There are not phrases records';
+		}
+
+		public function addHome($home){
+			array_push($this->home, $home);
+		}
+
+		public function getHome($number, $number_words = 0){
+			$home = $this->home;
+
+			if (is_array($home)){
+				if (isset($home[$number - 1])){
+					if ($number_words != 0)
+						return $this->getWords($home[$number - 1], $number_words);
+
+					return $home[$number - 1];
+				}
+
+				return 'This field does not exist';
+			}
+
+			return 'There are not home records';
+		}
+
+		public function addAbout($about){
+			array_push($this->about, $about);
+		}
+
+		public function getAbout($number, $number_words = 0){
+			$about = $this->about;
+
+			if (is_array($about)){
+				if (isset($about[$number - 1])){
+					if ($number_words != 0)
+						return $this->getWords($about[$number - 1], $number_words);
+
+					return $about[$number - 1];
+				}
+
+				return 'This field does not exist';
+			}
+
+			return 'There are not home records';
+		}
+
+		public function addService($title, $content){
+			array_push($this->service, ['title' => $title, 'content' => $content]);
+		}
+
+		public function getService($number, $number_words = 0){
+			$service = $this->service;
+
+			if (is_array($service)){
+				if (isset($service[$number - 1])){
+					if ($number_words != 0)
+						return ['title' => $this->service[$number -1]['title'],  'content' => $this->getWords($service[$number - 1]['content'], $number_words)];
+
+					return ['title' => $this->service[$number -1]['title'],  'content' => $this->service[$number -1]['content']];
+				}
+
+				$msg = 'This field does not exist';
+				return ['title' => $msg, 'content' => $msg];
+			}
+
+			$msg = 'There are not home records';
+			return ['title' => $msg, 'content' => $msg];
+		}
+
+		public function addReview($name, $location, $comment, $stars = 5){
+			array_push($this->review, ['name' 		=> $name, 
+									   'location' 	=> $location, 
+									   'comment' 	=> $comment, 
+									   'stars' 		=> $stars]
+			);
+		}
+
+		public function getReview($number, $number_words = 0){
+			$review = $this->review;
+
+			if (is_array($review)){
+				if (isset($review[$number - 1])){
+
+					$comment = $review[$number - 1]['comment'];
+
+					if ($number_words != 0)
+						$comment = $this->getWords($comment, $number_words);
+
+					return ['name' 		=> $this->review[$number -1]['name'],  
+							'location' 	=> $this->review[$number -1]['location'],
+							'comment' 	=> $comment, 
+							'stars' 	=> $this->review[$number -1]['stars']];
+
+				}
+
+				$msg = 'This field does not exist';
+				return ['name' => $msg, 'location' => $msg, 'comment' => $msg, 'stars' => $msg];
+			}
+
+			$msg = 'There are not home records';
+			return ['name' => $msg, 'location' => $msg, 'comment' => $msg, 'stars' => $msg];
+		}
 	}
--- app/model/text.php
+++ app/model/text.php
@@ -2,33 +2,107 @@
 	$Info = new Company();
 
 	$Info->company = 'Vazquez Painting and Remodeling';
-	$Info->address = 'Houston, TX';
+
+	$Info->experience 	= '13 Years of Experience';
+	$Info->estimates	= 'Free Estimates Are Available';
+	$Info->services 	= 'Residential and Commercial Services';
+	$Info->payment 		= 'We Accept Cash and Checks';
+
+	$Info->schedule 		= 'Monday to Saturday from 7 a.m. to 6 p.m.';
+	$Info->schedule_short	= 'Mon - Sat 7am to 6pm';
+
+	$Info->address 		= 'Houston, TX';
+	$Info->cover		= 'We Cover<br class="d-sm-none"> 80 Miles Around Houston';
+	$Info->googlemap	= '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d443087.54862369417!2d-95.68148741985043!3d29.817478217443124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C+Texas%2C+EE.+UU.!5e0!3m2!1ses!2sni!4v1555512550406!5m2!1ses!2sni" class="googlemap" allowfullscreen></iframe>';
+	
+	$Info->license		= 'We Are Licensed';
+	$Info->lic			= '';
 
 	$Info->addPhone('(832) 845-6401');
 	$Info->addPhone('(666) 666-6401');
 	$Info->addEmail('contact@vazquezpaintingandremodeling.com');
 	$Info->addEmail('israelsantos179@gmail.com');
 
-	$Info->services 	= 'Residential and Commercial Services';
-	$Info->payment 		= 'We Accept Cash and Checks';
-	$Info->experience 	= '13 Years of Experience';
+	$Info->addPhrase('Where Your Walls Come Back to Life');
+	$Info->addPhrase('Painting that Enhances Your Home’s Look and Feel');
+	$Info->addPhrase('Painting Is Our Passion');
+	$Info->addPhrase('Transform Your Space with the Best');
+	$Info->addPhrase('Let Us Upgrade Your Home!');
 
-	$Info->schedule 		= 'Monday to Saturday from 7 a.m. to 6 p.m.';
-	$Info->schedule_short	= 'Mon - Sat 7am to 6pm';
+	$Info->addHome('Make sure that all your walls and every room in your home reflect your style and preferences! With Vazquez Painting and Remodeling, the perfect look for your home or business is just a call away! We guarantee you that when you work with us, you’ll be completely satisfied with the results!.');
+	$Info->addHome('As a completely licensed company, customers in the area trust us to always remain professional and responsible in every project we undertake. Our priority is that we meet your needs beyond your expectations. You can expect the best customer service and quality craftsmanship from our team!.');
+	$Info->addHome('Vazquez Painting and Remodeling is an expert for both residential and commercial services. Call us if you ever need: ');
+	$Info->addHome('1. Interior and Exterior Painting<br/>
+				    2. Pressure Washing<br/>
+				    3. Kitchen and Bathroom Remodeling<br/>
+				    4. Drywall Repair<br/>
+				    5. Texture Painting<br/>
+				    6. Custom Painting<br/>
+				    7. Hardwood Floors<br/>
+				    8. Laminate Floors<br/>
+				    9. Ceramic Tile<br/>');
 
-	$Info->estimates	= 'Free Estimates Are Available';
-	$Info->cover		= 'We Cover<br class="d-sm-none"> 80 Miles Around Houston';
-	$Info->license		= 'We Are Licensed';
-	$Info->lic			= '';
+	$Info->addAbout('For more than 13 years, Vazquez Painting and Remodeling has been serving customers in Houston, Texas and surrounding areas with outstanding solutions and quality craftsmanship. When you work with us, we guarantee you will have the best experience during your painting or remodeling project.');
+	$Info->addAbout('Our company has maintained a reputation for always delivering the highest quality work and at affordable prices. We will be available during the whole project to answer any questions you might have. You can count on us all the time as we are very available and friendly.');
+	$Info->addAbout('Vazquez Painting and Remodeling abides by the following values: ');
+	$Info->addAbout('1. Quality work<br/>
+					 2. Punctuality<br/>
+					 3. Creativity<br/>
+					 4. Diligence<br/>
+					 5. Innovation');
 
-	$Info->googlemap	= '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d443087.54862369417!2d-95.68148741985043!3d29.817478217443124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C+Texas%2C+EE.+UU.!5e0!3m2!1ses!2sni!4v1555512550406!5m2!1ses!2sni" class="googlemap" allowfullscreen></iframe>';
+	$Info->addService('Interior and Exterior Painting', 'When you’re looking to add life to your faded walls, you need to turn to Vazquez Painting and Remodeling! We offer both interior and exterior painting so that you give your space the look that you’ve always wanted. Our crew uses the highest quality painting products to ensure we deliver the best results.');
+	$Info->addService('Pressure Washing', 'Make sure that your home’s exterior is always in the best condition with help from Vazquez Painting and Remodeling. Our team will ensure to get rid of all dust, dirt, mold, or even loose paint with our professional pressure washing services. We have the skills and equipment to ensure that your outdoors look in the best shape all the time.');
+	$Info->addService('Kitchen and Bathroom Remodeling', 'The kitchen and bathroom are two of the most important rooms in your home, and they should meet both your style and needs. Let the experts at Vazquez Painting and Remodeling help you achieve the look you’ve always wanted with our remodeling services. We guarantee you will love your new and beautiful bathroom and kitchen.');
+	$Info->addService('Drywall Repair', 'Patch up those holes and fix those cracks in your walls with Vazquez Painting and Remodeling! Our company offers professional drywall repair services that will leave all your walls looking as good as new. We use top-of-the-line materials and tools so that we deliver the best results.');
+	$Info->addService('Texture Painting', 'If you want to add a touch of style to your walls, then allow Vazquez Painting and Remodeling to help you! We are experts in texture painting and will ensure that all your walls look remarkable. Our team will work alongside you to understand what your needs are and deliver exactly that.');
+	$Info->addService('Other services:', 
+					  '1. Custom Painting<br/>
+					  2. Hardwood Floors<br/>
+					  3. Laminate Floors<br/>
+					  4. Ceramic Tile<br/>
+					  5. Granite Installation<br/>
+					  6. Cabinet Installation and Repair<br/>
+					  7. Siding Installation<br/>
+					  8. Concrete Work<br/>
+					  9. Roofing Installation and Repair<br/>
+					  10. Framing');
 
-	$Info->facebook	= '#';
-	$Info->twitter	= '#';
-	$Info->linkedin	= '#';
+	# addReviews(name, location, comment, stars)
+	$Info->addReview('Josh V.', 
+					  'Channelview, TX', 
+					  "Vazquez Painting and Remodeling does an excellent job and reasonably priced. You definitely get way more than your moneys worth. They repainted my deck and my garage door at my home. It gave the deck and the entire backyard a completely new updated fresh look. I recommend this business to anyone who needs painting done the right way.", 
+					  5);
+	$Info->addReview('Lana Rigsby', 
+					  'Vazquez Painting & Remodeling', 
+					  "I love working with Israel Sanntos and his men. They are responsible, careful and thorough workers who always do more than whats asked of them. They are trustworthy in all regards. I recommend them without hesitation.", 
+					  5);
+	$Info->addReview('Yesi', 
+					  'Cuba', 
+					  "Hacen un maravilloso trabajo y un gran equipo. Lo recomiendo al 100.", 
+					  5);
+
+
+
+	# SOCIAL NETWORKS
+	$Info->facebook			= '#';
+	$Info->twitter			= '#';
+	$Info->linkedin			= '#';
+	$Info->googlemybusiness = '#';
+	$Info->yelp				= 'https://www.yelp.com/biz/vazquez-painting-and-remodeling-houston';
+	$Info->homeadvisor		= '#';
+	$Info->buzzfile			= '#';
+	$Info->businessfinder	= '#';
+	$Info->manta			= '#';
+	$Info->yellowbot		= '#';
+	$Info->houzz			= '#';
+	$Info->dexknow			= '#';
 
 	#SEO
-	$Info->domain;
+	$Info->why				= '';
+	$Info->mission			= '';
+ 	$Info->vision			= '';
+	$Info->domain 			= '';
 	$Info->satisfaction		= '';
 	$Info->locality			= '';
 	$Info->region			= '';
@@ -38,6 +112,11 @@
 
 	// echo $Info->seo_convert;
 
+	// echo $Info->getPhrase(3);
+	// echo $Info->getHome(1, 15);
+	// echo $Info->getAbout(2, 10);
+	// echo $Info->getService(2, 30)['content'];
+	// echo $Info->getReview(2, 10)['comment'];
 	// echo $Info->getWords($Info->services, 20);
 	// echo $Info->getEmail(1)['Email'];
 	// echo $Info->getPhone(1)['PhoneRef'];
